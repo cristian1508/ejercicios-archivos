66238a289abe371e4dbc4b5df5bec3aca825b589
cristian1508 <93049602+cristian1508@users.noreply.github.com>
2023-02-06 16:26:51 -0600

    Add files via upload

---
+++ EjercicioArchivos/EjercicioArchivos/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioArchivos
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; } 
+        public string Location { get; set; }
+
+        public Person(string name, int age, string location)
+        {
+            Name = name;
+            Age = age;
+            Location = location;
+        }
+
+
+    }
+}
---
+++ EjercicioArchivos/EjercicioArchivos/Program.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EjercicioArchivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            int userNeed = 0;
+
+            while(userNeed == (int)YesorNOt.S)
+            {
+                Console.WriteLine($"Do you want enter a new person Yes = {(int)YesorNOt.S} No = {(int)YesorNOt.N}");
+
+                userNeed = Int32.Parse(Console.ReadLine());
+
+                if (userNeed != (int)YesorNOt.S) break;
+
+                Console.WriteLine("Write your name");
+
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Input your age");
+
+                int age = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter your location");
+
+                string location = Console.ReadLine();
+
+                Person per1 = new Person(name, age, location);
+
+                people.Add(per1);
+
+            } 
+
+            string personjson = JsonConvert.SerializeObject(people);
+
+            if (File.Exists("MiArchivo.txt"))
+            {
+                FileStream fs = new FileStream("MiArchivo.txt", FileMode.Append);
+
+                fs.Write(ASCIIEncoding.ASCII.GetBytes(personjson), 0, personjson.Length);
+
+                fs.Close();
+            }
+            else
+            {
+                FileStream fs = new FileStream("MiArchivo.txt", FileMode.OpenOrCreate);
+
+                fs.Write(ASCIIEncoding.ASCII.GetBytes(personjson), 0, personjson.Length);
+
+                fs.Close();
+            }
+
+            
+            
+        }
+    }
+    enum YesorNOt
+    {
+        S,
+        N
+    }
+}
